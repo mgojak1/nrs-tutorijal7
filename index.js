197f36f876f14f31ae3069c9bf25c1ad029387cd
mgojak1 <mgojak1@etf.unsa.ba>
2020-05-07 20:39:58 +0200

    index.js fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html").end();
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(8080);
